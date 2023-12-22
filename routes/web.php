1eed9f63f01eb512249d789679ad6fe0f6349c72
BaselHosari88 <bassel.housri88@gmail.com>
2023-12-22 15:25:53 +0100

    Authentication Laravel

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\ListingController;
+use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+
+Route::get('/listings', [ListingController::class, 'index']);
+
+Route::get('/listings/create', [ListingController::class, 'create'])->middleware('auth');
+
+Route::post('/listings', [ListingController::class, 'store'])->middleware('auth');
+
+Route::get('listings/{listing}/edit', [ListingController::class, 'edit'])->middleware('auth');
+
+Route::put('listings/{listing}', [ListingController::class, 'update'])->middleware('auth');
+
+Route::delete('listings/{listing}', [ListingController::class, 'delete'])->middleware('auth');
+
+Route::get('/listings/{listing}', [ListingController::class, 'show']);
+
+
+require __DIR__ . '/auth.php';
